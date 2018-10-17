92a93be1af427ee028c97aa16a712064c84787fa
Martincz Gao <martincz@mokeedev.com>
2018-10-17 15:19:46 +0800

    wayne: Derp
    Change-Id: I7cf7923d8ebd011b82ef79c0546065fe70ff9a58

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-mk
+    $(LOCAL_PATH)/overlay-mokee
 
 # Permissions
 PRODUCT_COPY_FILES += \
