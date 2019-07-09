TeamWin/android_device_google_crosshatch

Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: I7f06edb331a2a56f29db26294b2ece28666d2103

AndroidProducts.mk
@@ -17,3 +17,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_crosshatch.mk \
     $(LOCAL_DIR)/aosp_blueline.mk \
+
+COMMON_LUNCH_CHOICES := \
+    aosp_crosshatch-userdebug \
+    aosp_blueline-userdebug

MoKee/android_device_xiaomi_wayne

Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

AndroidProducts.mk
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mk_wayne.mk
+
+COMMON_LUNCH_CHOICES := \
+    mk_wayne-eng \
+    mk_wayne-userdebug