af2f4c75e95eea7358251577c8de9f729b3a95fe
PIPIPIG233666 <2212848813@qq.com>
2019-07-10 22:35:57 -0400

    wayne: Clean up & optimize.
     * Add missing blobs (Factory,SSR,etc).
     * Kang Audio FX modules from perseus-user 9 PKQ1.180729.001 V10.3.1.0.PEECNXM release-keys too.
     * Inspired by https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/9185320eab1aa755d82d89e5fb06a011601c2249.
     * Silense a power-libperfmgr warning in the way `line 79` did.
     * Slightly re-order proprietary files list.
    
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: I7f8b82f3f4ffeff457274f37482059015ebca77d

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,21 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-EXTRA_VENDOR_LIBRARIES_32 := \
-    libaudioclient \
-    libaudiomanager \
-    libbinder \
-    libcamera_client \
-    libft2 \
-    libgui.vendor \
-    libharfbuzz_ng \
-    libheif \
-    libicui18n \
-    libicuuc \
-    libmedia \
-    libmediaextractor \
-    libmediametrics \
-    libminikin \
-    libsonivox
+include $(LOCAL_PATH)/vndk-ext-libs.mk
 
 define define-vndk-lib
 include $$(CLEAR_VARS)
---
+++ vndk/vndk-ext-libs.mk
@@ -0,0 +1,16 @@
+EXTRA_VENDOR_LIBRARIES_32 := \
+    libaudioclient \
+    libaudiomanager \
+    libbinder \
+    libcamera_client \
+    libft2 \
+    libgui.vendor \
+    libharfbuzz_ng \
+    libheif \
+    libicui18n \
+    libicuuc \
+    libmedia \
+    libmediaextractor \
+    libmediametrics \
+    libminikin \
+    libsonivox
